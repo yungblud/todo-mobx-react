c68643936f5703cf3b2ae550e2ac5cb68ef166f3
Dongho Choi <kkiathorn@gmail.com>
2020-02-06 18:03:47 +0900

    set wrappers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import TodoWrapper from './components/TodoWrapper';
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: calc(100vh);
+`;
+
+const Title = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 80px;
+
+  border-bottom: 1px solid #e9ecef;
+`;
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <TodoWrapper>
+        <Title>Todo Mobx</Title>
+      </TodoWrapper>
+    </AppWrapper>
   );
 };
 
---
+++ src/components/TodoWrapper.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 650px;
+  height: auto;
+
+  background: #ffffff;
+  border-radius: 2px;
+  display: inline-block;
+  margin: 1rem;
+  position: relative;
+`;
+
+interface Props {
+  children: any;
+}
+
+const TodoWrapper = ({ children }: Props) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default TodoWrapper;
